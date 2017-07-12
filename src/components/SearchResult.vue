11c946e1f0f02c4630b17758d65f5246ad37781b
Mattias Knutsson <mattias.knutsson@pensionsmyndigheten.se>
2017-07-12 12:21:52 +0200

    Snyggar om sökresultatet

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -1,27 +1,40 @@
 <template>
-  <ul>
-    <li v-for="res in result" v-on:click="res.detailed=!res.detailed">
-      <div class="header">
+  <ul class="result">
+    <li v-for="res in result" v-on:click="res.detailed=!res.detailed" class="result-item">
+      <div class="preview">
         <div>
           <h3>
             {{res.LegList|time}} {{res.dur}} min
           </h3>
-          <h5>
-            <span>{{res|origin}}</span>
-            <span>-</span>
-            <span>{{res|destination}}</span>
-          </h5>
+          <div class="preview-sub">
+            <h5>
+              <span>{{res|origin}}</span>
+              <span>-</span>
+              <span>{{res|destination}}</span>
+            </h5>
+            <div class="preview-sub-leg" v-for="part in getRealLegs(res)">
+              <img v-bind:src="part.type | typeImg" class="preview-sub-leg-img"></img>
+              <span class="preview-sub-leg-text">{{part.line}}</span>
+            </div>
+          </div>
         </div>
         <button class="expander">+</button>
       </div>
-      <div class="legList" v-show="res.detailed">
+      <div class="body" v-show="res.detailed">
         <template v-for="part in getRealLegs(res)">
-          <div v-bind:alt="part.name">
-            <img v-bind:src="part.type | typeImg" width="50"></img>
+          <div v-bind:alt="part.name" class="body-leg">
             <p class="time">{{part.Origin.time}}</p>
-            <span class="line">{{part.line}}</span>
+            <div class="body-leg__icon">
+              <img v-bind:src="part.type | typeImg" class="body-leg__icon-img">
+              <span class="body-leg__icon-text">{{part.line}}</span>
+              </img>
+            </div>
+            <div>
+              <p>Mot: {{part.dir}}</p>
+              <p v-if="part !== res.LegList.Leg[res.LegList.Leg.length-1]">Byt vid: {{part.Destination.name}}</p>
+            </div>
           </div>
-          <p class="middle-station" v-if="part !== res.LegList.Leg[res.LegList.Leg.length-1]">{{part.Destination.name}}</p>
+  
         </template>
       </div>
     </li>
@@ -53,49 +66,81 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-ul {
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-}
-
-li {
-  margin: 0 10px;
-  padding: 10px 0;
-  border-bottom: 1px solid black;
-}
-
-.legList {
-  display: flex;
-  justify-content: center;
-  margin-top: 1em;
-}
-
-.legList>* {
-  position: relative;
-  padding: 5px;
-}
-
-.line {
-  position: absolute;
-  top: 33px;
-  left: 0;
-  right: 0;
-}
-
-h3 {
-  margin: 5px;
-}
-
+<style scoped lang="scss">
+h3,
 h5,
 p {
   margin: 0;
 }
 
-.header {
-  position: relative;
+.result {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+
+  &-item {
+    margin: 0 10px;
+    padding: 10px 0;
+    border-bottom: 1px solid black;
+  }
 }
+
+.preview {
+  position: relative;
+  &-sub {
+    margin-right: 2em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &-leg {
+      margin: 0 0.5rem;
+      position: relative;
+      &-img {
+        width: 2rem;
+      }
+      &-text {
+        position: absolute;
+        top: calc(50%);
+        left: 0;
+        right: 0;
+        font-size: 0.7em;
+      }
+    }
+  }
+}
+
+.body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 1em;
+
+  &-leg {
+    display: flex;
+    align-items: center;
+    position: relative;
+
+    &>*{
+      margin:0.5rem;
+    }
+
+    &__icon {
+      position: relative;
+      &-img {
+        width: 3rem;
+      }
+      &-text {
+        position: absolute;
+        top: calc(50%);
+        left: 0;
+        right: 0;
+        font-size: 1.05em;
+      }
+    }
+  }
+}
+
 
 .expander {
   position: absolute;
